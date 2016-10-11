01a126c57f73f329667f3eb480633a714e57e8e9
Eli Goodfriend <eli.goodfriend@gmail.com>
2016-10-11 14:32:13 -0400

    uses weights to find more accurate estimate of most common industry

--- industry_by_PUMA.R
+++ industry_by_PUMA.R
@@ -1,5 +1,4 @@
 # what is the most common industry in each PUMA region?
-# TODO adjust using ACS magic repeated values
 library("ff")
 library("ffbase")
 library("maptools")
@@ -11,11 +10,28 @@ people <- read.csv.ffdf(file=filename)
 industryAlone <- people$industry
 idx <- which(!is.na(industryAlone)[])
 industryAlone <- industryAlone[idx] # only entries that have an industry
-industryTable <- sort(table(industryAlone), decreasing=TRUE)
-cat("The most common three industries are:\n")
+# unweighted
+industryTable <- sort(table(industryAlone), decreasing=TRUE) # unweighted most common industries
+cat("The most common three industries are (unweighted):\n")
 cat(names(industryTable[1]), ", with", as.character(industryTable[1]), "workers,")
 cat(names(industryTable[2]), ", with", as.character(industryTable[2]), "workers, and")
 cat(names(industryTable[3]), ", with", as.character(industryTable[3]), "workers.")
+# weighted
+weights <- people$weight[idx]
+industryTypes <- sort(unique(industryAlone))
+numIndustry <- length(industryTypes)
+industryCounts <- integer(numIndustry)
+for (i in seq(1,numIndustry)){
+  thisIndustry <- industryTypes[i]
+  industryIndices <- which(industryAlone==thisIndustry)
+  industryCounts[i] <- industryCounts[i] + sum(weights[industryIndices])
+}
+allIndustryCounts <- data.frame(industryTypes, industryCounts)
+allIndustryCounts <- allIndustryCounts[order(-industryCounts),]
+cat("The most common three industries are (weighted):\n")
+cat(as.character(allIndustryCounts$industryTypes[1]), ", with", as.character(allIndustryCounts$industryCounts[1]), "workers,")
+cat(as.character(allIndustryCounts$industryTypes[2]), ", with", as.character(allIndustryCounts$industryCounts[2]), "workers, and")
+cat(as.character(allIndustryCounts$industryTypes[3]), ", with", as.character(allIndustryCounts$industryCounts[3]), "workers.")
 
 # construct unique geography codes from state and PUMA
 PUMA_ram <- as.ram(people$PUMA[idx])
@@ -24,12 +40,34 @@ states_ram <- as.ram(people$state[idx])
 states_ram <- sprintf("%02d",as.numeric(states_ram))
 geography <- paste(states_ram,PUMA_ram,sep="")
 
-# what is the most common industry in each PUMA region?
+# what is the most common industry in each PUMA region? 
+# unweighted
 geographyIndustryTable <- table(geography, industryAlone)
 maxIndustryIdx <- apply(geographyIndustryTable,1,which.max)
 maxIndustry <- industryAlone[maxIndustryIdx]
 maxIndustryDF <- as.data.frame(maxIndustry)
 maxIndustryDF$geography <- rownames(as.data.frame(maxIndustryIdx))
+# weighted TODO can this vectorize?
+geographyTypes <- unique(geography)
+numGeography <- length(geographyTypes)
+maxIndustryByGeography <- integer(numGeography)
+for (i in seq(1,numGeography)){
+  geographyIndices <- which(geography==geographyTypes[i])
+  relevantIndustries <- industryAlone[geographyIndices]
+  relevantWeights <- weights[geographyIndices]
+  uniqueRelevantIndustries <- unique(relevantIndustries)
+  numUniqueRelInd <- length(uniqueRelevantIndustries)
+  relevantIndustryCounts <- integer(numUniqueRelInd)
+  for (j in seq(1,numUniqueRelInd)){ # TODO refactor c/p
+    thisIndustry <- relevantIndustries[j]
+    relevantIndustryCounts[j] <- relevantIndustryCounts[j] + sum(relevantWeights[which(relevantIndustries==thisIndustry)])
+  }
+  allIndustryCounts <- data.frame(uniqueRelevantIndustries, relevantIndustryCounts)
+  allIndustryCounts <- allIndustryCounts[order(-relevantIndustryCounts),]
+  maxIndustryByGeography[i] <- allIndustryCounts$uniqueRelevantIndustries[1]
+}
+maxIndustryDF$maxIndustry <- maxIndustryByGeography
+maxIndustryDF$geography <- geographyTypes
 
 # pull out the industry names
 setOfMaxIndustries <- sort(unique(maxIndustryDF$maxIndustry))
@@ -38,7 +76,8 @@ industryNames <- read.table("/home/eli/Data/ACS/industries.txt", sep=".")
 maxIndustryNamesIdx <- match(setOfMaxIndustries,industryNames$V1)
 maxIndustryNames <- industryNames$V2[maxIndustryNamesIdx]
 
-# plot a map of the results
+# plot a map of the results: all industries separated
+# this is illegible
 maxIndustryDF$maxIndustry <- as.factor(maxIndustryDF$maxIndustry) # to make discrete color plot
 library("ggplot2")
 library("mapproj")
@@ -51,8 +90,10 @@ plotOfMap <- ggplot() +
   geom_polygon(data = allPUMAregions.df,
               aes(x = long, y = lat, group = group, fill = maxIndustry)) +
   coord_map() +
-  theme_nothing(legend=TRUE) +
+  theme_nothing(legend=FALSE) +
   xlim(-130, -65) +
   ylim(25,50) +
   scale_fill_hue("Most common industry in region",l = 40,labels = maxIndustryNames)
 ggsave(plotOfMap, file="prettyPlot.png", type="cairo-png", width=20, height=10)
+
+# TODO plot a map of the results: grouping industries by type
