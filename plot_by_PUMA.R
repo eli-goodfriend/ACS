02f0dde436e2d072f99eff9336812f15a04cb8e5
Eli Goodfriend <eli.goodfriend@gmail.com>
2016-10-17 13:53:27 -0400

    AK and HI are also in America

--- plot_by_PUMA.R
+++ plot_by_PUMA.R
@@ -4,19 +4,44 @@ library("ffbase")
 library("maptools")
 library("plyr")
 
-plotMap <- function(dataframe, column, legendText, fileName){    
+plotMap <- function(dataframe, column, titleText, fileName){    
   library("ggplot2")
   library("mapproj")
   library("ggmap")
-  plotOfMap <- ggplot() +
+  library("gridExtra")
+  limits = c(min(column),max(column))
+  continentalPlot <- ggplot() +
     geom_polygon(data = dataframe,
                  aes(x = long, y = lat, group = group, fill = column)) +
     coord_map() +
     theme_nothing(legend=TRUE) +
+    theme(legend.text=element_text(size=20),
+          plot.title=element_text(size=24)) +
+    labs(title=titleText) +
     xlim(-130, -65) +
     ylim(25,50) +
-    scale_fill_gradient(legendText)
-  ggsave(plotOfMap, file=fileName, type="cairo-png", width=20, height=10)
+    scale_fill_gradient("",low="white", high="blue", limits=limits)
+  alaskaPlot <- ggplot() +
+    geom_polygon(data = dataframe,
+                 aes(x = long, y = lat, group = group, fill = column)) +
+    coord_map() +
+    theme_nothing(legend=FALSE) +
+    xlim(-180, -130) +
+    ylim(53,73) +
+    scale_fill_gradient(low="white", high="blue", limits=limits)
+  hawaiiPlot <- ggplot() +
+    geom_polygon(data = dataframe,
+                 aes(x = long, y = lat, group = group, fill = column)) +
+    coord_map() +
+    theme_nothing(legend=FALSE) +
+    xlim(-160, -153) +
+    ylim(17.5,22.5) +
+    scale_fill_gradient(low="white", high="blue", limits=limits)
+  usaPlot <- grid.arrange(arrangeGrob(alaskaPlot + theme(legend.position="none"),
+                                      hawaiiPlot + theme(legend.position="none"),
+                                      nrow=2),
+                          continentalPlot, nrow=1, widths=c(1,5))
+  ggsave(usaPlot, file=fileName, type="cairo-png", width=20, height=10)
 }
 
 filename <- "/home/eli/Data/ACS/people.ff"
