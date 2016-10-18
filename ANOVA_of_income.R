0cd29720bd1757925f13eab47721195ca79edc10
Eli Goodfriend <eli.goodfriend@gmail.com>
2016-10-18 15:52:54 -0400

    Wilcoxon says stay in school

--- ANOVA_of_income.R
+++ ANOVA_of_income.R
@@ -34,19 +34,37 @@ cvm.test(income[which(gender==2)]) # not even close
 # we really shouldn't use a two sample t test, but what if we do anyway?
 t.test(income~gender, var.equal=FALSE) # means are significantly different
 t.test(income~gender, var.equal=FALSE, alternative="greater") # men have a significantly greater income than women
-# t test was not appropriate, what if we use the Wilcocon-Mann-Whitney test instead?
+# t test was not appropriate, what if we use the Wilcoxon-Mann-Whitney test instead?
 wilcox.test(income~gender) # data are significantly different
 wilcox.test(income~gender, alternative="greater") # men have a significantly greater income than women
 
-# what about for eduation? more than two education groups, so we need one-way ANOVA
+# Is there a difference in income between a high school diploma and a GED?
+# let's just skip to the non-parametric test
+HSidx <- which(education==16 | education==17)
+HSincome <- income[HSidx]
+HSeducation <- factor(education[HSidx])
+boxplot(HSincome~HSeducation, notch=TRUE, varwidth=TRUE, outline=FALSE,
+        ylab="Total income ($/year)",
+        names=c("Diploma","GED"))
+wilcox.test(HSincome~HSeducation, alternative="greater") # diploma is better
+
+# Is there a difference in income between finishing high school and not finishing college?
+SCidx <- which(education==18 | education==19)
+SCincome <- income[SCidx]
+boxplot(HSincome, SCincome, notch=TRUE, varwidth=TRUE, outline=FALSE,
+        ylab="Total income ($/year)",
+        names=c("High school","Some college"))
+wilcox.test(HSincome, SCincome, alternative="less") # some college is better
+
+# what about for eduation overall? more than two education groups, so we need one-way ANOVA
 # SPOILERS: yes income is significantly different for different education levels
 # preprocess education to make more meaningful bins and order the factors
 source("~/Dropbox/Code/ACS/preprocess/group_education.R")
 education <- group_education(education)
 boxplot(income~education, notch=TRUE, varwidth=TRUE, outline=FALSE,
-        ylab="Total income ($/year)",
+        ylab="Total income ($/year)", las=3,
         names=c("No diploma or GED","High school","Some college",
-                "Assoc.","Bach.","Mast.","Professional","PhD"))
+                "Associates","Bachelors","Masters","Professional","Doctoral"))
 # use Bartlett's K-squared to determine if variances are homogeneous
 bartlett.test(income~education) # no, they are not
 # TODO are the data normally distributed? that seems very unlikely, but can check
@@ -62,5 +80,8 @@ summary(fit_alt) # seems like same results as using lm and anova
 kruskal.test(income~education) # yup, education generates significantly different income
 
 
-
-
+# what happens when gender and education are considered together?
+# let's briefly pretend gender and education are independent
+# TODO test that assumption
+boxplot(income~gender*education, notch=TRUE, varwidth=TRUE, outline=FALSE,
+        ylab="Total income ($/year)", las=3)
