67a622c32642e30a595fbd516dd3306be0dbe219
Eli Goodfriend <eli.goodfriend@gmail.com>
2016-10-18 11:44:54 -0400

    t test of gender, but not adjusted for weights

--- ANOVA_of_income.R
+++ ANOVA_of_income.R
@@ -1,23 +1,38 @@
-# explore the predictors of income using ANOVA
+# explore the predictors of income using t-tests and ANOVA
 # TODO how to incorporate weights and repeated observations?
 library("ff")
 library("ffbase")
 library("ggplot2")
 library("stats")
+library("nortest")
 
 filename <- "/home/eli/Data/ACS/people.ff"
 acsData <- read.csv.ffdf(file=filename)
 
 income <- as.ram(acsData$totalIncome)
-indices <- which(!is.na(income))
+indices <- which(!is.na(income)) # only people who have an income
 income <- income[indices]
 
 gender <- as.factor(as.ram(acsData$gender[indices]))
 people <- data.frame(income, gender)
 
-# does gender generate a significant different in total income?
-# since the ACS has two genders, this gives two groups and we can use a two sample t test
-
+# does gender predict a significant difference in total income?
+# SPOILERS: yes DOUBLE SPOILERS: women have less income than men
+# since the ACS has two genders, this gives two groups and we can potential use a two sample t test
+# test for t-test assumptions:
+# are the variances the same? use Fisher's F test
+var.test(income~gender) # no, they are not, since p < 0.05
+# are distributions close to normal? use the Cramer-von Mises test for normality
+# total income is signed, so it can be negative
+cvm.test(income) # not even close
+cvm.test(income[which(gender==1)]) # not even close
+cvm.test(income[which(gender==2)]) # not even close
+# we really shouldn't use a two sample t test, but what if we do anyway?
+t.test(income~gender, var.equal=FALSE) # means are significantly different
+t.test(income~gender, var.equal=FALSE, alternative="greater") # men have a significantly greater income than women
+# t test was not appropriate, what if we use the Wilcocon-Mann-Whitney test instead?
+wilcox.test(income~gender) # data are significantly different
+wilcox.test(income~gender, alternative="greater") # men have a significantly greater income than women
 
 # what about for eduation? more than two education groups, so we need ANOVA
 # what about using R's stats package to do this?
